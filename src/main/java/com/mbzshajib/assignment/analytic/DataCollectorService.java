ababa1e8e2587ebda38b70987390cd8f3cea2719
Shajib <mbzshajib@gmail.com>
2022-03-07 01:02:05 +0600

    refactor -renammed

--- src/main/java/com/mbzshajib/assignment/analytic/DataCollectorService.java
+++ src/main/java/com/mbzshajib/assignment/analytic/DataCollectorService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TickAnalyticServiceApplication {
+public class DataCollectorService {
 
     public static void main(String[] args) {
-        SpringApplication.run(TickAnalyticServiceApplication.class, args);
+        SpringApplication.run(DataCollectorService.class, args);
     }
 
 }
