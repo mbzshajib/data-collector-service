07fe6b7f578e51cdf1dae07e7edf1ed578a60de9
Shajib <mbzshajib@gmail.com>
2022-03-07 23:43:59 +0600

    handle exception with exception resolver

--- src/main/java/com/mbzshajib/assignment/analytic/excptionresolver/TickControllerAdvice.java
+++ src/main/java/com/mbzshajib/assignment/analytic/excptionresolver/TickControllerAdvice.java
@@ -2,9 +2,10 @@ package com.mbzshajib.assignment.analytic.excptionresolver;
 
 import com.mbzshajib.assignment.analytic.controller.TickController;
 import com.mbzshajib.assignment.analytic.controller.TickStatisticController;
+import com.mbzshajib.assignment.analytic.exception.FutureRequestException;
+import com.mbzshajib.assignment.analytic.exception.NoProcessingRequiredException;
 import com.mbzshajib.assignment.analytic.exception.ServerOverloadedException;
 import com.mbzshajib.assignment.analytic.model.ErrorResponse;
-import com.mbzshajib.assignment.analytic.utils.ErrorCodes;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static com.mbzshajib.assignment.analytic.utils.ErrorCodes.*;
 
 @Slf4j
 @ControllerAdvice(basePackageClasses = {
@@ -22,39 +25,47 @@ public class TickControllerAdvice {
     private String UNKNOWN_ERROR_MSG = "Unknown error happened";
 
     @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({MethodArgumentNotValidException.class})
-    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        return ErrorResponse.builder()
-                .errorCode(ErrorCodes.INVALID_REQUEST_DATA)
-                .errorMessage(exception.getFieldError().getDefaultMessage())
-                .build();
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    @ExceptionHandler(ServerOverloadedException.class)
+    public void handleServerOverloadedException(ServerOverloadedException exception) {
     }
 
     @ResponseBody
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ExceptionHandler(NoProcessingRequiredException.class)
+    public void handleNoProcessingRequiredException(NoProcessingRequiredException exception) {
+    }
+
+
+    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({IllegalArgumentException.class})
-    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException exception) {
+    @ExceptionHandler(FutureRequestException.class)
+    public ErrorResponse handleFutureRequestException(FutureRequestException exception) {
         log.error("exception occurred. ", exception);
         return ErrorResponse.builder()
-                .errorCode(ErrorCodes.INVALID_REQUEST)
+                .errorCode(TICK_FROM_FUTURE)
                 .errorMessage(exception.getMessage())
                 .build();
     }
 
     @ResponseBody
-    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
-    @ExceptionHandler({ServerOverloadedException.class})
-    public void handleServerOverloadedException(ServerOverloadedException exception) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+
+        return ErrorResponse.builder()
+                .errorCode(INVALID_REQUEST_DATA)
+                .errorMessage(exception.getFieldError().getDefaultMessage())
+                .build();
     }
 
     @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler({Exception.class})
+    @ExceptionHandler(Exception.class)
     public ErrorResponse handleCommonError(Exception exception) {
         log.error("Unknown exception occurred. ", exception);
         return ErrorResponse.builder()
-                .errorCode(ErrorCodes.UNKNOWN_ERROR)
+                .errorCode(UNKNOWN_ERROR)
                 .errorMessage(UNKNOWN_ERROR_MSG)
                 .build();
     }
